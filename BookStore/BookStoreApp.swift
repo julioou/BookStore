11683048a593a2436cec1799d9f7085f20276b9f
julioou <54035853+julioou@users.noreply.github.com>
2024-01-29 20:24:13 +0000

    Initial commit

---
+++ BookStore/BookStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookStoreApp.swift
+//  BookStore
+//
+//  Created by Júlio Santos on 29/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct BookStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
